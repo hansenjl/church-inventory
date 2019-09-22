9a25bba794eb08447b869ca8b186c3fd9c0ce4aa
hansenjl <jenn.leigh.hansen@gmail.com>
2019-09-21 21:41:25 -0700

    add validations for name

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,6 @@
 class Item < ApplicationRecord
   belongs_to :area
   belongs_to :purpose
+
+  vvalidates :name, presence: true
 end
--- app/models/purpose.rb
+++ app/models/purpose.rb
@@ -1,3 +1,6 @@
 class Purpose < ApplicationRecord
   belongs_to :church
+  has_many :items
+
+  validates :name, presence: true
 end
